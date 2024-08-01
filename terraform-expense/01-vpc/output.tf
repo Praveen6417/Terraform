b9e0d02f9086c1b1d7b7af0223c620860255a4fa
Praveen6417 <praveenkasagalla123@gmail.com>
2024-08-01 18:31:49 +0530

    added output.tf file to terraform-expense

--- terraform-expense/01-vpc/output.tf
+++ terraform-expense/01-vpc/output.tf
@@ -1,43 +1,43 @@
-output "availability_zones_list" {
-  value = data.aws_availability_zones.availability_zone.names
-}
+# output "availability_zones_list" {
+#   value = data.aws_availability_zones.availability_zone.names
+# }
 
 output "vpc" {
-  value = aws_vpc.expense.id
+  value = module.aws_vpc.id 
 }
 
-output "length_public_subnets" {
-  value = length(aws_subnet.expense-public[*].id)
-}
+# output "length_public_subnets" {
+#   value = length(aws_subnet.expense-public[*].id)
+# }
 
-output "default_vpc_id" {
-  value = data.aws_vpc.default_vpc.id
-}
+# output "default_vpc_id" {
+#   value = data.aws_vpc.default_vpc.id
+# }
 
-output "default_route_vpc" {
-  value = data.aws_route_table.default_route_vpc.id
-}
+# output "default_route_vpc" {
+#   value = data.aws_route_table.default_route_vpc.id
+# }
 
-output "associated_subnet_ids" {
-  value = aws_route_table_association.public_route[*].subnet_id
-}
+# output "associated_subnet_ids" {
+#   value = aws_route_table_association.public_route[*].subnet_id
+# }
 
-output "public_subnet_ids" {
-  value = aws_subnet.expense-public[*].id
-}
+# output "public_subnet_ids" {
+#   value = aws_subnet.expense-public[*].id
+# }
 
-output "private_subnet_ids" {
-  value = aws_subnet.expense-private[*].id
-}
+# output "private_subnet_ids" {
+#   value = aws_subnet.expense-private[*].id
+# }
 
-output "private_db_subnet_ids" {
-  value = aws_subnet.expense-private-db[*].id
-}
+# output "private_db_subnet_ids" {
+#   value = aws_subnet.expense-private-db[*].id
+# }
 
-output "private_db_subnet_group_id" {
-  value = aws_db_subnet_group.db_group.id
-}
+# output "private_db_subnet_group_id" {
+#   value = aws_db_subnet_group.db_group.id
+# }
 
-output "private_db_subnet_group_name" {
-  value = aws_db_subnet_group.db_group.name
-}
+# output "private_db_subnet_group_name" {
+#   value = aws_db_subnet_group.db_group.name
+# }
