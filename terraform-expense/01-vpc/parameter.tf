b770d1380a4e6b58090f430c263b574e7973d7f7
Praveen6417 <praveenkasagalla123@gmail.com>
2024-08-01 19:24:06 +0530

    added parameters.tf file to terraform-expense

--- terraform-expense/01-vpc/parameter.tf
+++ terraform-expense/01-vpc/parameter.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "vpc_id" {
   type = "String"
   value = module.aws_vpc.vpc
 }
+
+resource "aws_ssm_parameter" "public_subnets" {
+  name = "/${var.project_name}/${var.environment}/public_subnets"
+  type = "StringList"
+  value = module.aws_vpc.public_subnet_ids
+}
