965da42faed8699cb73612968944392eaa020c6e
Praveen6417 <praveenkasagalla123@gmail.com>
2024-08-02 00:34:07 +0530

    added data.tf, output.tf and parameters.tf to 02-sg terraform-expense

---
+++ terraform-expense/02-sg/output.tf
@@ -0,0 +1,12 @@
+# output "aws_ssm_parameter_vpc_id" {
+#   value = data.aws_ssm_parameter.vpc_id.value
+#   sensitive = true
+# }
+
+# output "module_db" {
+#   value = module.db.id
+# }
+
+# output "module_db_sg_id" {
+#   value = module.db.sg_id
+# }
