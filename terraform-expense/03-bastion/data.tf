2a1974183065d8b79b11df4ab67441a3eb35d303
Praveen6417 <praveenkasagalla123@gmail.com>
2024-08-02 02:03:21 +0530

    This is a sample commit

--- terraform-expense/03-bastion/data.tf
+++ terraform-expense/03-bastion/data.tf
@@ -3,7 +3,7 @@ data "aws_ssm_parameter" "bastion_sg_id" {
 }
 
 data "aws_ssm_parameter" "public_subnet_ids" {
-  name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+  name = "/${var.project_name}/${var.environment}/public_subnets"
 }
 
 data "aws_ami" "ami_id" {
