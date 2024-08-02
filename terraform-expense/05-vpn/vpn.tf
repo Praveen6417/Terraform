534dc6d26c449c731bc63a86bd08f3f686572d81
Praveen6417 <praveenkasagalla123@gmail.com>
2024-08-02 16:17:12 +0530

    Added 05-vpn repo

---
+++ terraform-expense/05-vpn/vpn.tf
@@ -0,0 +1,22 @@
+module "vpn" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  key_name = aws_key_pair.public_key.key_name
+  name = "${var.project_name}-${var.environment}-vpn"
+
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [ data.aws_ssm_parameter.vpn_sg_id.value ]
+  ami = data.aws_ami.ami_id.id
+  subnet_id              = local.public_subnet_ids
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project_name}-${var.environment}-vpn"
+    }
+  )
+}
+
+resource "aws_key_pair" "public_key" {
+  key_name = "vpn"
+  public_key = file("pub.txt")
+}
