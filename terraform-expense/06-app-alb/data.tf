2efa682f296c8f3ab48c9eb04dff9e6e2b716f73
Praveen6417 <praveenkasagalla123@gmail.com>
2024-08-02 17:11:38 +0530

    Added 06-app-alb repo

--- terraform-expense/06-app-alb/data.tf
+++ terraform-expense/06-app-alb/data.tf
@@ -3,7 +3,7 @@ data "aws_ssm_parameter" "vpn_sg_id" {
 }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+  name = "/${var.project_name}/${var.environment}/private_subnets"
 }
 
 data "aws_ssm_parameter" "app-alb_sg_id" {
