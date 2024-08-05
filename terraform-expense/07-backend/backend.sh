a13ef658a9bdee78979f03358a417e4e425cf670
Praveen6417 <praveenkasagalla123@gmail.com>
2024-08-05 17:00:50 +0530

    Updated 07-Backend Repo

--- terraform-expense/07-backend/backend.sh
+++ terraform-expense/07-backend/backend.sh
@@ -2,4 +2,4 @@
 
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/Praveen6417/Ansible.git -d Ansible /Ansible/Ansible-roles-Practice/main.yml -e component=$component -e env=$environment
+ansible-pull -i localhost, -U https://github.com/Praveen6417/Ansible.git -d /home/ec2-user/Ansible Ansible-roles-Practice/main.yml -e component=$component -e env=$environment
